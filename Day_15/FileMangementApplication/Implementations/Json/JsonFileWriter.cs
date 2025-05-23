440d38f22d4663e2e82245780c8f7c737d2a398f
Moumitha22 <moumitharaghu22@gmail.com>
2025-05-23 21:23:31 +0530

    Update file writer and reader with 'using'

--- Day_15/FileMangementApplication/Implementations/Json/JsonFileWriter.cs
+++ Day_15/FileMangementApplication/Implementations/Json/JsonFileWriter.cs
@@ -5,6 +5,18 @@ public class JsonFileWriter : IFileWriter
 {
     public void Write(string filePath, string content)
     {
+        // StreamWriter writer = null;
+        // try
+        // {
+        //     writer = new StreamWriter(filePath);
+        //     writer.Write(content);
+        // }
+        // finally
+        // {
+        //     if (writer != null)
+        //         writer.Dispose(); // Closes the file
+        // }
+        
         using (StreamWriter writer = new StreamWriter(filePath))
         {
             writer.Write(content);
--- Day_15/FileMangementApplication/Implementations/Text/TextFileWriter.cs
+++ Day_15/FileMangementApplication/Implementations/Text/TextFileWriter.cs
@@ -5,6 +5,18 @@ public class TextFileWriter : IFileWriter
 {
     public void Write(string filePath, string content)
     {
+        
+        // StreamWriter writer = null;
+        // try
+        // {
+        //     writer = new StreamWriter(filePath);
+        //     writer.Write(content);
+        // }
+        // finally
+        // {
+        //     if (writer != null)
+        //         writer.Dispose(); // Closes the file
+        // }
         using (StreamWriter writer = new StreamWriter(filePath))
         {
             writer.Write(content);
